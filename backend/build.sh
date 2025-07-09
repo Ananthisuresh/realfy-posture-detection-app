c1355ce43efa220b09aaf0f0968becfabb5cd87c
Ananthisuresh <ananthi101203@gmail.com>
2025-07-09 08:54:38 +0530

    Fix: install mediapipe directly via requirements.txt

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Upgrade pip and dependencies
-pip install --upgrade pip setuptools wheel
-
-# Install everything from requirements.txt first
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Then install mediapipe manually from GitHub
-pip install protobuf==3.20.3
-pip install git+https://github.com/google/mediapipe.git
